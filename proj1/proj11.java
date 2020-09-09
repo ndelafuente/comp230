c67adea393674810986b805a419029fe7f9b8c44
Dillan Lopez <dillanlopez@sandiego.edu>
2020-09-08 20:46:32 -0700

    finished part1

--- proj1/proj11.java
+++ proj1/proj11.java
@@ -4,29 +4,38 @@ import java.util.Scanner;
 public class proj11{
     public static int[] keyValues;
     public static int numberOfKeyValues;
+
     public static void main(String[] args) {
-        
         Scanner scanInts = new Scanner(System.in);
         Scanner scanStr = new Scanner(System.in);
-        System.out.println("Enter the number of values you wish to use.");
-        numberOfKeyValues = scanInts.nextInt();
-        keyValues = new int[numberOfKeyValues];
-        System.out.println("Enter individual values sepereated by spaces.");
+        Scanner scanQuit = new Scanner(System.in);
+        String runProgram = "y";
+        while(runProgram.charAt(0) == 'y'){
+            System.out.println("Enter the number of values you wish to use.");
+            numberOfKeyValues = scanInts.nextInt();
+            keyValues = new int[numberOfKeyValues];
+            System.out.println("Enter individual values sepereated by spaces.");
 
-        int i = 0;
-        while(i < numberOfKeyValues) {
-            keyValues [i] = scanInts.nextInt();
-            i++;
+            int i = 0;
+            while(i < numberOfKeyValues) {
+                keyValues [i] = scanInts.nextInt();
+                i++;
+            }
+
+            System.out.println("Enter the message you wish to encrypt:");
+            String phrase =  scanStr.nextLine();
+
+            String encryptedPhrase = encrypt(phrase);
+            System.out.println("Your encrypted message is: "+ encryptedPhrase);
+
+            String decryptedPhrase = decrypt(encryptedPhrase);
+            System.out.println("Your decrypted message is: "+ decryptedPhrase);
+
+            System.out.println("Do you want to run the program again y or n?");
+            runProgram =  scanQuit.nextLine();
+
         }
-
-        System.out.println("Enter the message you wish to encrypt:");
-        String phrase =  scanStr.nextLine();
-
-        String encryptedPhrase = encrypt(phrase);
-        System.out.println("Your encrypted message is: "+ encryptedPhrase);
-
-        String decryptedPhrase = decrypt(encryptedPhrase);
-        System.out.println("Your decrypted message is: "+ decryptedPhrase);
+        scanQuit.close();
         scanInts.close();
         scanStr.close();
     }
@@ -50,5 +59,4 @@ public class proj11{
         }
         return decryptedMessage;
         }
-
 }
