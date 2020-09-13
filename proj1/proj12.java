952a6448ac7ffbdfc92662ffe2708addb67389fc
Nicolas de la Fuente <ndelafuente@sandiego.edu>
2020-09-12 23:23:40 -0700

    edited output spacing for proj12

--- proj1/proj12.java
+++ proj1/proj12.java
@@ -1,7 +1,7 @@
 /**
  * proj12.java
  * 
- * This program takes in a file of sorted integers and creates a new fie where
+ * This program takes in a file of sorted integers and creates a new file where
  * any duplicate entries are removed.
  * 
  * Authors: Dillan Lopez and Nicolas de la Fuente
@@ -15,7 +15,7 @@ public class proj12 {
     public static BufferedReader inStream; // the input stream
     public static BufferedWriter outStream; // the output stream
     public static void main(String[] args) {
-        char running = 'y'; // variable to keep track of whether the user wants to run again
+        char running = 'y'; // keep track of whether the user wants to run again
         Scanner s = new Scanner(System.in);
         try {
             while (running == 'y') {
@@ -24,19 +24,20 @@ public class proj12 {
                     // Get the input and output file names
                     System.out.print("Enter the input file name: ");
                     inFilename = s.nextLine();
-                    System.out.print("Enter the output file name (leave blank to use the same file): ");
+                    System.out.print("Enter the output file name: ");
                     outFilename = s.nextLine();
+					System.out.println();
 
                     // Create the buffers from the file names
                     inStream = new BufferedReader(new FileReader(inFilename));
-                    if (outFilename.length() <= 1) outFilename = inFilename;
                     outStream = new BufferedWriter(new FileWriter(outFilename));
                     
                     // Read each line until the end of the file
                     String line = inStream.readLine();
                     while (line != null) {
-                        int num = Integer.valueOf(line); // test whether the line is a valid int
-                        outStream.write(Integer.toString(num) + '\n'); // write the number to the file
+                        int num = Integer.valueOf(line); // throws exception if not an int
+						// Write the number to the file
+                        outStream.write(Integer.toString(num) + '\n');
 
                         String nextLine = inStream.readLine();
                         if(nextLine == null) break; // stop reading if there is no next line
@@ -49,6 +50,8 @@ public class proj12 {
                         }
                         line = nextLine;
                     }
+					System.out.println("Successfully removed duplicates from '" + inFilename + "'!");
+					System.out.println("Updated data can be found in '" + outFilename + "'\n");
                     outStream.close();
 
                 }
@@ -77,6 +80,7 @@ public class proj12 {
             ex.printStackTrace();
             System.exit(1);
         }
+		System.out.println("Goodbye!");
         s.close();
     } // end of main
 }
