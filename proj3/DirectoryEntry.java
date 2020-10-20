f6f61d69c157f78c2904ce2ad97a02e9407f1d32
Dillan Lopez <dillanlopez@sandiego.edu>
2020-10-20 15:53:29 -0700

    worked on project

--- proj3/DirectoryEntry.java
+++ proj3/DirectoryEntry.java
@@ -1,4 +1,11 @@
 public class DirectoryEntry {
     String number;
     String name;
+    DirectoryEntry(String name, String number){
+        name = this.name;
+        number = this.number;
+    }
+    public String toString(){
+        return name.toString() + " "+ number.toString();
+    }
 }
